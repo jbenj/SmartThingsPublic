61c3e0a8db9116245bf036c765cce9e1ac14e740
jbenj <james.benjamin@mac.com>
2017-05-16 13:02:18 -0500

    Update centralite-keypad.groovy

--- devicetypes/mitchpond/centralite-keypad.src/centralite-keypad.groovy
+++ devicetypes/mitchpond/centralite-keypad.src/centralite-keypad.groovy
@@ -1,7 +1,7 @@
 /**
  *  Centralite Keypad
  *
- *  Copyright 2015-2016 Mitch Pond, Zack Cornelius
+ *  Copyright 2015-2016 Mitch Pond, Zack Cornelius.b
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
